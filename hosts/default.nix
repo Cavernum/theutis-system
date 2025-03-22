fb8effc09b1934501ecab653104b0136ed009b70
Safenein <safenein@cavernum.ovh>
2025-03-22 21:47:10 +0100

    fix: testing firewall rules

--- hosts/default.nix
+++ hosts/default.nix
@@ -8,7 +8,7 @@
   ];
 
   networking.hostName = "theutis";
-  networking.firewall.allowedTCPPorts = [ 22 ];
+  networking.firewall.allowedTCPPorts = [ 22 80 8009 443 ];
   networking.networkmanager.enable = true;
 
   time.timeZone = "Europe/Paris";
